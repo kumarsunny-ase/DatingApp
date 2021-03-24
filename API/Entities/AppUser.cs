426470175227ce78574baaaaacf7f90471900ae1
Kumar Sunny <kumarsunny1348@gmail.com>
2021-03-25 00:26:58 +0600

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.Entities
@@ -6,5 +7,7 @@ namespace API.Entities
     {
          public int Id { get; set; }
         public string UserName { get; set; }
+        public Byte[] PasswordHash { get; set; }
+        public Byte[] PasswordSalt { get; set; }
     }
 }
